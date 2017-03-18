5dbf7ede34a778dcf363cead7f5af0839aa57c37
hongming <hongming@gmail.com>
2017-03-18 18:49:05 +0800

    计算公式调整

--- BinocularsHUD.ino
+++ BinocularsHUD.ino
@@ -1,5 +1,5 @@
 /**
-d  float精度不够，可考虑arduino due
+float精度不够，可考虑arduino due
   计算公式来源：
   1，http://www.fjptsz.com/xxjs/xjw/rj/117/12.htm
   地平坐标转到赤道坐标
@@ -63,37 +63,23 @@ void setup() {
   Serial.println("Caculating.........");
   //提供观测者基础数据
   //观测者所在纬度
-  Latitude = 36.10;
+  Latitude = 31.0456;
   //观测者所在经度
-  Longitude = 120.34;
+  Longitude = 121.3997;
 
 }
 void loop() {
- 
-
-  //以下获得JY901实时方位角和地平维度基础数据
 
 
- //获得JY901角度数据
-//Serial.print("Angle:"); 
-//  Serial.print(-1*(float)JY901.stcAngle.Angle[0] / 32768 * 180); 
-//  float az=(float)JY901.stcAngle.Angle[2] / 32768 * 180;
-//  if (az<=0)
-//    {az=-1*az;}
-//  else
-//    {az=360-az;}
-//  Serial.print(" "); Serial.println(az);
-
-  //JY901数据（ROLL,PITCH,YAW）与A方位角/地平纬度的关系变换
+  //以下获得JY901实时方位角和地平维度基础数据,JY901数据（ROLL,PITCH,YAW）与A方位角/地平纬度的关系变换
 
 float jy_pitch=(float)JY901.stcAngle.Angle[0]/32768*180;
 
 float jy_yaw=(float)JY901.stcAngle.Angle[2] / 32768 * 180;
- if (jy_yaw<=0)
-    {jy_yaw=-1*jy_yaw;}
-  else
-    {jy_yaw=360-jy_yaw;}
-
+// if (jy_yaw<=0)
+//    {jy_yaw=-1*jy_yaw;}
+//  else
+//    {jy_yaw=360-jy_yaw;}
 
 Serial.print("Azimuth");
 Serial.print(jy_yaw);
@@ -102,11 +88,12 @@ Serial.print("Altitude");
 Serial.print(jy_pitch);
 Serial.print("         ");
   //测试用方位角（弧度），获取和计算,假设为0
-  Azimuth = jy_yaw*2 * PI / 360;
+//  Azimuth = jy_yaw*2 * PI / 360;
+  Azimuth=218.061611*2 * PI / 360;
 
   //测试用地平纬度（弧度），获取和计算，假设为65度
-  Altitude = jy_pitch* 2 * PI / 360;
-
+//  Altitude = jy_pitch* 2 * PI / 360;
+  Altitude=66.642722* 2 * PI / 360;
   //以下获得实时时间
   /*
     RTC获取数据
@@ -114,22 +101,22 @@ Serial.print("         ");
 
   //测试用时间
   Year = 2017;
-  Month = 2;
-  Day = 25;
-  Hour = 20;
+  Month = 3;
+  Day = 17;
+  Hour = 23;
   Hour = Hour - 8;
   // Serial.println(Hour);
-  Minute = 0;
+  Minute = 20;
   Second = 0;
 
   //儒略日，计算采用Navy.mil的计算试试看
   JD = 367 * Year - int((7 * (Year + int((Month + 9) / 12))) / 4) + int((275 * Month) / 9) + Day + 1721013.5 + Hour / 24 + Minute / 1440 + Second / 86400 - 0.5 * ((((100 * Year + Month - 190002.5) > 0) - ((100 * Year + Month - 190002.5) < 0))) + 0.5;
-  Serial.print("JD is   ");
-  Serial.println(JD, 6);
+//  Serial.print("JD is   ");
+//  Serial.print(JD, 6);
   //简化儒略日，计算
   MJD = JD - 2400000.5;
-  Serial.print("MJD is   ");
-  Serial.println(MJD, 6);
+//  Serial.print("     MJD is   ");
+//  Serial.println(MJD, 6);
   //格林尼治恒星时
   //    Siderial_Time = 6.697374558 + 0.06570982441908 * (JD - Hour / 24 - 2451545.0) + 1.00273790935 * Hour + 0.000026 * (JD - 2451545.0) * (JD - 2451545.0) / (36525 * 36525);
   //http://aa.usno.navy.mil/faq/docs/GAST.php
@@ -138,36 +125,44 @@ Serial.print("         ");
     Siderial_Time += 24.0;
   while (Siderial_Time > 24.0)
     Siderial_Time -= 24.0;
-  Serial.print("GST is: ");
-  Serial.print("  ");
-  Serial.print(Siderial_Time, 6);
-  Serial.print("  ");
+//  Serial.print("GST is: ");
+//  Serial.print("  ");
+//  Serial.print(Siderial_Time, 6);
+//  Serial.print("  ");
   //本地恒星时
    Siderial_Time_Local = Siderial_Time + Longitude/15;
+//Siderial_Time_Local = Siderial_Time + 8.0;
 
   while (Siderial_Time_Local < 0.0)
     Siderial_Time_Local += 24.0;
   while (Siderial_Time_Local > 24.0)
     Siderial_Time_Local -= 24.0;
-  Serial.print("LST is: ");
-  Serial.print("  ");
-  Serial.print(Siderial_Time_Local, 6);
-  Serial.print("  ");
+//  Serial.print("LST is: ");
+//  Serial.print("  ");
+//  Serial.print(Siderial_Time_Local, 6);
+//  Serial.print("  ");
   //计算赤经
-  Astro_HUD_RA = Siderial_Time_Local-atan(sin(Azimuth) / ( cos(Azimuth) * sin(Latitude*(2*PI/360)) + tan(Altitude) * cos(Latitude*(2*PI/360)) ))*180/(PI*15) ;
+  Astro_HUD_RA = Siderial_Time_Local-atan(sin(Azimuth) / ( cos(Azimuth) * sin(Latitude*(2*PI/360)) - tan(Altitude) * cos(Latitude*(2*PI/360)) ))*180/(PI*15) ;
   //计算赤纬δ = 赤纬。天赤道以北为正，以南为负。
   Astro_HUD_DEC = asin(sin(Latitude*2*PI/360) * sin(Altitude) + cos(Latitude*(2*PI/360)) * cos(Altitude) * cos(Azimuth))*360/(2*PI);
   // Siderial_Time_Local * 15 -
   //* 360 / (2 * PI) ;
   Serial.print("RA:");
-  Serial.print(Astro_HUD_RA);
-  Serial.print("  ");
-  Serial.print("Dec:");
-  Serial.println(Astro_HUD_DEC);
-  delay(2000);
-    while (Serial1.available()) 
+//  Serial.print(Astro_HUD_RA);
+//    Serial.print("  ");
+  Serial.print(int(floor(Astro_HUD_RA)));
+  Serial.print("h");
+  Serial.print(round((Astro_HUD_RA-floor(Astro_HUD_RA))*60));  
+  Serial.print("m");
+  Serial.print(" Dec:");
+  Serial.print(int(floor(Astro_HUD_DEC)));
+  Serial.print("°");
+  Serial.print(round((Astro_HUD_DEC-floor(Astro_HUD_DEC))*60));  
+   Serial.println("'");
+  delay(500);
+    while (Serial1.available())
   {
     JY901.CopeSerialData(Serial1.read()); //Call JY901 data cope function
   }
-  
+
 }
